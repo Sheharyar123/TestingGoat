b6ddc1a4a00f0060805189a56047ca543ad92aed
Sheharyar Ahmad <phantomwitch35@gmail.com>
2022-09-19 21:58:44 +0500

    Use unittest module to write first real test'

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import unittest
 
-s=Service('C:/Users/Phantom Lover/Downloads/Dev/chromedriver.exe')
-browser = webdriver.Chrome(service=s)
-url='http://localhost:8000'
-browser.get(url)
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        s=Service('C:/Users/Phantom Lover/Downloads/Dev/chromedriver.exe')
+        self.browser = webdriver.Chrome(service=s)
+
+    def tearDown(self):
+        return self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To Do', self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
